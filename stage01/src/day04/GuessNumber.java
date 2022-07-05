08f56d1c9c75a142051fed0513dd4a0952e7331f
lingnian <sytsnb@gmail.com>
2022-07-05 17:21:47 +0800

    day04-3

--- stage01/src/day04/GuessNumber.java
+++ stage01/src/day04/GuessNumber.java
@@ -4,11 +4,37 @@ import java.util.Random;
 import java.util.Scanner;
 
 /**
- * 猜数字
+ * 猜数字--while
+ * Math.random() 0~0.9999999-----
+ * <p>
+ * do...while ,先执行后判断，至少执行一次
  */
 public class GuessNumber {
     public static void main(String[] args) {
-        guessNumber();
+//        System.out.println((int) (Math.random() * 100 + 1));
+
+//        guessNumber();
+        guessNumber2();
+    }
+
+    private static void guessNumber2() {
+        Random random = new Random();
+        int numRan = random.nextInt(100) + 1;
+        System.out.println(numRan);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("猜数字----1~100之内：");
+        int i;
+        do {
+            i = sc.nextInt();
+            if (i > numRan)
+                System.out.println("猜大了！！！！");
+            else if (i < numRan)
+                System.out.println("猜小了！！！！");
+            else {
+                System.out.println("猜对了！！！");
+//                break;
+            }
+        } while (i != numRan);
     }
 
     private static void guessNumber() {
