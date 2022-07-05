a760b2cd90755c27518dd5a7df8d484fe7285137
lingnian <sytsnb@gmail.com>
2022-07-05 09:08:05 +0800

    if else

--- test01/src/day03/IfElseDemo.java
+++ test01/src/day03/IfElseDemo.java
@@ -5,10 +5,22 @@ package day03;
  */
 public class IfElseDemo {
     public static void main(String[] args) {
-        if (true){
+        int score = 540;
+        if (score >= 0 && score <= 100) {
+            System.out.println("成绩合法");
+        } else {
+            System.out.println("成绩不合法");
+        }
 
-        }else {
 
+        /*
+        &&  优先级大于 或  ||
+         */
+        int year = 2001;
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println("闰年");
+        } else {
+            System.out.println("平年   ");
         }
     }
 }
