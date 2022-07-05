d98b5250d75461d53a60b5646ce417d85c827f64
lingnian <sytsnb@gmail.com>
2022-07-05 11:11:26 +0800

    switch

--- test01/src/day04/ScannerDemo.java
+++ test01/src/day04/ScannerDemo.java
@@ -8,13 +8,25 @@ import java.util.Scanner;
 public class ScannerDemo {
     public static void main(String[] args) {
         ScoreLevel();
-        CommandBySwitch();
+//        CommandBySwitch();
     }
 
     private static void ScoreLevel() {
         Scanner sc = new Scanner(System.in);
         System.out.println("输入成绩：");
         double score = sc.nextDouble();
+        if (score > 100 || score < 0)
+            System.out.println("成绩不合法！！");
+        else if (score >= 90)
+            System.out.println("优秀！！");
+        else if (score >= 80)
+            System.out.println("良好！！");
+        else if (score >= 70)
+            System.out.println("不错！！");
+        else if (score >= 60)
+            System.out.println("及格！！");
+        else
+            System.out.println("不及格");
         System.out.println("成绩为：" + score);
     }
 
---
+++ test01/src/day04/SwitchDemo.java
@@ -0,0 +1,23 @@
+package day04;
+
+/**
+ * Switch ...case
+ */
+public class SwitchDemo {
+    public static void main(String[] args) {
+        int num =1;
+        switch (num){ //byte ,shot ,char ,int
+            case 1:
+                System.out.println(1);
+                break;
+            case 2:
+                System.out.println(2);
+                break;
+            case 3:
+                System.out.println(3);
+                break;
+            default:
+                System.out.println("default!!");
+        }
+    }
+}
