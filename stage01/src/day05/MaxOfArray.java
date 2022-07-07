8feb0d3c3820e87148192ddc4f635dd8da45be9f
lingnian <sytsnb@gmail.com>
2022-07-07 09:54:45 +0800

    'for' loop replaceable with enhanced 'for'

--- stage01/src/day05/MaxOfArray.java
+++ stage01/src/day05/MaxOfArray.java
@@ -11,9 +11,9 @@ public class MaxOfArray {
         int max = -1;
         int min = 100;
 
-        for (int i = 0; i < arr.length; i++) {
-            max = Math.max(arr[i], max);
-            min = Math.min(arr[i], min);
+        for (int j : arr) {
+            max = Math.max(j, max);
+            min = Math.min(j, min);
         }
         System.out.println("最大值：" + max);
         System.out.println("最小值：" + min);
