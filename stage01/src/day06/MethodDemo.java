2a4f47d8ab8632b098b32ef16f0c7a02e48905fe
lingnian <sytsnb@gmail.com>
2022-07-07 16:34:09 +0800

    method

--- stage01/src/day06/MethodDemo.java
+++ stage01/src/day06/MethodDemo.java
@@ -6,11 +6,42 @@ package day06;
 public class MethodDemo {
     public static void main(String[] args) {
 
-
+        int[] arr = generateArray();
+        ArrayToString(arr);
+        /*
+        System.out.println(plus(5, 6));
         System.out.println(getDouble());
-//        say();
-//        say("ling");
-//        say("lingNian", 22);
+        say();
+        say("ling");
+        say("lingNian", 22);
+        */
+    }
+
+    private static int[] generateArray() {
+//        生成数组- 容量- 10
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 100);
+        }
+        return arr;
+    }
+
+    private static void ArrayToString(int[] arr) {
+        //打印数组 - [,,,,]
+        System.out.printf("[");
+        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = (int) (Math.random() * 100);
+            if (i != arr.length - 1)
+                System.out.print(arr[i] + ",");
+            else
+                System.out.print(arr[i]);
+        }
+        System.out.println("]");
+    }
+
+    private static int plus(int num1, int num2) {
+        int num = num1 + num2;
+        return num;
     }
 
     private static double getDouble() {
