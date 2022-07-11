867701361627309fa8668ac3ac9e727f99178be8
lingnian <sytsnb@gmail.com>
2022-07-11 11:29:54 +0800

    this--

--- stage01/src/ooday02/ConsDemo.java
+++ stage01/src/ooday02/ConsDemo.java
@@ -1,4 +1,21 @@
 package ooday02;
 
+import java.util.LinkedList;
+
 public class ConsDemo {
+    public static void main(String[] args) {
+        Student zs = new Student();
+        zs.name = "张三";
+        zs.age = 18;
+        zs.address = "西湖";
+        zs.sayHi();
+
+        Student ls = new Student("李四", 19, "余杭");
+        ls.sayHi();
+
+//        this("---", 0, "南京");  this()
+        Student stu = new Student();
+        stu.sayHi();
+        LinkedList list =new LinkedList<>();
+    }
 }
--- stage01/src/ooday02/Student.java
+++ stage01/src/ooday02/Student.java
@@ -1,4 +1,26 @@
 package ooday02;
 
-public class Student {
+/**
+ * 学生类
+ * java: 成员变量和局部变量可以同名，使用时默认采取就近原则
+ * 若想访问成员变量，使用 this-------this访问成员变量（成员变量与局部变量同名时）
+ */
+class Student {
+    String name; //成员变量（整个类）
+    int age;
+    String address;
+
+    Student() {
+        this("---", 0, "南京");
+    }
+
+    Student(String name, int age, String address) {//局部变量（当前方法中）
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    void sayHi() {
+        System.out.println("name: " + name + " age: " + age + " address: " + address);
+    }
 }
