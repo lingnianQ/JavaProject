dae9eaa4f6a376d9cd3e6617ebe92014c0c7026e
lingnian <sytsnb@gmail.com>
2022-07-13 17:02:31 +0800

    swing

--- submarine/src/com/tedu/submarine/World.java
+++ submarine/src/com/tedu/submarine/World.java
@@ -1,19 +1,33 @@
 package com.tedu.submarine;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import java.util.ArrayList;
 
 /**
  * main
  */
-public class World {
+public class World extends JPanel {
     public static void main(String[] args) {
+        /**
+         * swing 图形化页面
+         */
+        JFrame frame = new JFrame();
+        World world = new World();
+        world.setFocusable(true);
+        frame.add(world);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(641 + 16, 479 + 39);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
 
         ArrayList<ObserverSubmarine> list = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
             ObserverSubmarine s = new ObserverSubmarine();
             list.add(s);
         }
-//        list.forEach(System.out::println);
+        list.forEach(System.out::println);
 
         SeaObject[] seaObjects = new SeaObject[]{
                 new ObserverSubmarine(),
@@ -38,8 +52,8 @@ public class World {
         }
 
         Bomb[] bs = new Bomb[2];
-        bs[0] = new Bomb(100,201);
-        bs[1] = new Bomb(111,201);
+        bs[0] = new Bomb(100, 201);
+        bs[1] = new Bomb(111, 201);
         for (Bomb b : bs) {
             System.out.println(b);
             b.move();
