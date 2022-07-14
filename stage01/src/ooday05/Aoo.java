e245c5bcb6439d2159b18bbbde532d9743549736
lingnian <sytsnb@gmail.com>
2022-07-14 11:48:16 +0800

    final

--- stage01/src/ooday05/Aoo.java
+++ stage01/src/ooday05/Aoo.java
@@ -6,6 +6,11 @@ package ooday05;
  * public  任何类
  * protect 本类，派生类，同包类---跨包继承
  * default 本类，同包类
+ * <p>
+ * final :
+ * 修饰变量：变量不能被改变
+ * 修饰方法：方法不能被重写--可以被重载
+ * 修饰类：类不能被继承
  */
 public class Aoo {
 }
