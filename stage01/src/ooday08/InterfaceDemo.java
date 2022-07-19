feefd282802efca5c70ab3633d62443777afc321
lingnian <sytsnb@gmail.com>
2022-07-19 15:07:51 +0800

    interface

---
+++ stage01/src/ooday08/InterfaceDemo.java
@@ -0,0 +1,14 @@
+package ooday08;
+
+/**
+ * 接口的演示
+ */
+public class InterfaceDemo {
+    public static void main(String[] args) {
+
+    }
+}
+interface Inter{
+    int COUNT = 5;
+    void test();
+}
