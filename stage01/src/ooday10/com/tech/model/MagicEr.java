cc7fafdd84f12defa03b13f7de98131612f884fe
lingnian <sytsnb@gmail.com>
2022-07-22 09:19:01 +0800

    game

--- stage01/src/ooday10/com/tech/model/MagicEr.java
+++ stage01/src/ooday10/com/tech/model/MagicEr.java
@@ -3,4 +3,13 @@ package ooday10.com.tech.model;
 public class MagicEr extends Person{
     private int level = 1;
 
+    @Override
+    public int attack() {
+        return 0;
+    }
+
+    @Override
+    public void practise() {
+
+    }
 }
--- stage01/src/ooday10/com/tech/model/Soldier.java
+++ stage01/src/ooday10/com/tech/model/Soldier.java
@@ -1,4 +1,13 @@
 package ooday10.com.tech.model;
 
-public class Soldier extends Person{
+public class Soldier extends Person {
+    @Override
+    public int attack() {
+        return 0;
+    }
+
+    @Override
+    public void practise() {
+
+    }
 }
